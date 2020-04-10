c8e3945c633fb8cc37c9484bd4bb75e72a74979e
Christoph Klein <codecircuit@gpu-acceleration.com>
2020-04-10 19:00:57 +0200

    Fix: generate_uniform_real signature

--- include/thrustshift/random.h
+++ include/thrustshift/random.h
@@ -1,24 +1,23 @@
 #pragma once
 
 #include <random>
+#include <type_traits>
 
 namespace thrustshift {
 
 namespace random {
 
-template <class Range, typename T = typename Range::value_type>
-void generate_uniform_real(Range&& range,
-                           T min = T{0},
-                           T max = T{1},
-                           unsigned long long seed = 0) {
-
-	std::default_random_engine rng(seed);
-	std::uniform_real_distribution<T> dist(min, max);
-	for (size_t i = 0; i < range.size(); ++i) {
-		range[i] = dist(rng);
-	}
+template <class Range,
+	  typename T = typename std::remove_reference<Range>::type::value_type>
+void generate_uniform_real(Range&& range, T min = T{0}, T max = T{1},
+			   unsigned long long seed = 0) {
+    std::default_random_engine rng(seed);
+    std::uniform_real_distribution<T> dist(min, max);
+    for (size_t i = 0; i < range.size(); ++i) {
+	range[i] = dist(rng);
+    }
 }
 
-} // namespace random
+}  // namespace random
 
-} // namespace thrustshift
+}  // namespace thrustshift
