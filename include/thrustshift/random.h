5f9aea69279463fc6a4a92b6b316763f90b16849
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2020-07-03 13:57:36 +0200

    random normal distribution

--- include/thrustshift/random.h
+++ include/thrustshift/random.h
@@ -7,17 +7,35 @@ namespace thrustshift {
 
 namespace random {
 
-template <class Range,
-	  typename T = typename std::remove_reference<Range>::type::value_type>
-void generate_uniform_real(Range&& range, T min = T{0}, T max = T{1},
-			   unsigned long long seed = 0) {
-    std::default_random_engine rng(seed);
-    std::uniform_real_distribution<T> dist(min, max);
-    for (size_t i = 0; i < range.size(); ++i) {
-	range[i] = dist(rng);
-    }
+template <class Range, class Distribution, class Engine>
+void generate(Range&& range, Distribution& dist, Engine& rng) {
+	for (size_t i = 0; i < range.size(); ++i) {
+		range[i] = dist(rng);
+	}
 }
 
-}  // namespace random
+template <class Range,
+          typename T = typename std::remove_reference<Range>::type::value_type>
+void generate_uniform_real(Range&& range,
+                           T min = T{0},
+                           T max = T{1},
+                           unsigned long long seed = 0) {
+	std::default_random_engine rng(seed);
+	std::uniform_real_distribution<T> dist(min, max);
+	generate(std::forward<Range>(range), dist, rng);
+}
 
-}  // namespace thrustshift
+template <class Range,
+          typename T = typename std::remove_reference<Range>::type::value_type>
+void generate_normal_real(Range&& range,
+                          T mean = T{0},
+                          T stddev = T{1},
+                          unsigned long long seed = 0) {
+	std::default_random_engine rng(seed);
+	std::normal_distribution<T> dist(mean, stddev);
+	generate(std::forward<Range>(range), dist, rng);
+}
+
+} // namespace random
+
+} // namespace thrustshift
