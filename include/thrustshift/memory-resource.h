016249ec8f718a97ee7b002649e3581b8bbf34ec
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-02-18 17:54:05 +0100

    Add non-oversubscribing memory pool resource class

--- include/thrustshift/memory-resource.h
+++ include/thrustshift/memory-resource.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <map>
+#include <vector>
 #include <memory_resource>
+
+#include <gsl-lite/gsl-lite.hpp>
 
 #include <cuda/runtime_api.hpp>
 
@@ -57,6 +60,8 @@ class device_resource_type : public std::pmr::memory_resource {
  *  is called it actually results into a new allocation if not the exact same
  *  size was allocated previously.
  *
+ *  Do not use if different buffers of the same size are required.
+ *
  *  \note std::pmr compatible
  */
 template <class Upstream>
@@ -99,6 +104,93 @@ class oversubscribed_delayed_pool_type : public std::pmr::memory_resource {
 	Upstream res_;
 	// size in bytes -> (ptr, alignment)
 	std::map<size_t, book_page_type> book_;
+};
+
+/*! \brief Allocates only if requested buffer size is currently not in the free pool.
+ *
+ *  This pool is useful for functions, which require temporary GPU memory. The
+ *  host can allocate memory via this pool, launch the kernel with the corresponding
+ *  pointer and exit the function without deallocating the memory because the
+ *  host is not aware about the runtime of the GPU kernel and when the kernel
+ *  may read the temporary required memory. This pool should not be used if the
+ *  byte size of the allocations differ often becaues every time an allocation
+ *  is called it actually results into a new allocation if not the exact same
+ *  size was allocated previously.
+ *  
+ *  Contrary to `oversubscribed_delayed_pool_type` this resource does not return
+ *  the same buffer if `deallocate` was not called for that buffer previously. This
+ *  is useful if functions delegate the memory resource to other functions, which
+ *  are maybe called iteratively and if the function requires different buffers of
+ *  the same size.
+ *
+ *  \note std::pmr compatible
+ */
+template <class Upstream>
+class delayed_pool_type : public std::pmr::memory_resource {
+   private:
+
+	struct page_item_type {
+		void* ptr;
+		size_t alignment;
+		bool allocated;
+	};
+
+   public:
+	delayed_pool_type() = default;
+
+	~delayed_pool_type() noexcept {
+		for (auto& [bytes, book_page] : book_) {
+			for (auto& page_item : book_page) {
+				res_.deallocate(page_item.ptr, bytes, page_item.alignment);
+			}
+		}
+	}
+
+   private:
+	void* do_allocate(size_t bytes, size_t alignment) override {
+		if (auto it = book_.find(bytes); it != book_.end()) {
+			for (auto& page_item : it->second) {
+				if (!page_item.allocated) {
+					page_item.allocated = true;
+					return page_item.ptr;
+				}
+			}
+			// no page item was free
+			void* ptr = res_.allocate(bytes, alignment);
+			it->second.push_back({ptr, alignment, true});
+			return ptr;
+		}
+		// the required size was never allocated before
+		void* ptr = res_.allocate(bytes, alignment);
+		book_[bytes] = {{ptr, alignment, true}};
+		return ptr;
+	}
+
+	void do_deallocate(void* ptr,
+	                   size_t bytes,
+	                   size_t alignment) noexcept override {
+		if (ptr == nullptr) {
+			return;
+		}
+		for (auto& page_item : book_[bytes]) {
+			if (page_item.ptr == ptr && page_item.allocated) {
+				gsl_Expects(page_item.alignment == alignment);
+				page_item.allocated = false;
+				return;
+			}
+		}
+		// The pointer which should be deallocated was never allocated before
+		std::terminate();
+	}
+
+	bool do_is_equal(
+	    const std::pmr::memory_resource& other) const noexcept override {
+		return this == &other;
+	}
+
+	Upstream res_;
+	// size in bytes -> (ptr, alignment)
+	std::map<size_t, std::vector<page_item_type>> book_;
 };
 
 } // namespace pmr
