bd36c3fcfa7088f95932830a165c879732e6f904
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-02-20 19:14:08 +0100

    Add eigen3 interface

---
+++ include/thrustshift/eigen3-interface/CSR.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+
+#include <gsl-lite/gsl-lite.hpp>
+
+#include <Eigen/Sparse>
+
+#include <thrustshift/CSR.h>
+#include <thrustshift/managed-vector.h>
+
+namespace thrustshift {
+
+namespace eigen {
+
+template <class EigenSparseMatrix>
+auto sparse_mtx2csr(EigenSparseMatrix&& m_) {
+
+	using DataType =
+	    typename std::remove_reference<EigenSparseMatrix>::type::value_type;
+	using StorageIndex =
+	    typename std::remove_reference<EigenSparseMatrix>::type::StorageIndex;
+	Eigen::SparseMatrix<DataType, Eigen::RowMajor, StorageIndex> m = m_;
+	m.makeCompressed();
+	auto m_data = m.data();
+	auto nnz = m.nonZeros();
+	auto rows = m.rows();
+
+	const DataType* A = &m_data.value(0);
+	const StorageIndex* IA = m.outerIndexPtr();
+	const StorageIndex* JA = &m_data.index(0);
+
+	managed_vector<DataType> seq_A(A, A + nnz);
+	managed_vector<StorageIndex> seq_JA(JA, JA + nnz);
+	managed_vector<StorageIndex> seq_IA(IA, IA + rows + 1);
+
+	return CSR<DataType,StorageIndex>(seq_A, seq_JA, seq_IA, gsl_lite::narrow<size_t>(m.cols()));
+}
+
+} // namespace eigen
+
+} // namespace thrustshift
