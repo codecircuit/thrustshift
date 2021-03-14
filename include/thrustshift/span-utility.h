d59a435b77c47877fcf61da52a99d0414c59fa69
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-03-14 13:45:24 +0100

    Comment + clang-format

--- include/thrustshift/span-utility.h
+++ include/thrustshift/span-utility.h
@@ -7,26 +7,47 @@
 
 namespace thrustshift {
 
-template <class T,
-          class RangeOfRanges,
-          class GetRangeCallback>
+/*! \brief Make multiple spans from a range of objects which contain ranges.
+ *
+ *  \param r
+ *  \param get_range This functor must **not** return a copy of any object
+ *
+ *  ```cpp
+ *  struct S {
+ *
+ *    std::vector<int> v;
+ *  };
+ *  ...
+ *
+ *  std::vector<S> ss;
+ *  // fill ss
+ *
+ *  //// WRONG
+ *  auto spans_with_invalid_ptrs = make_spans_from_ranges(ss, [](const auto& e) { return e.v; });
+ *
+ *  //// CORRECT
+ *  auto spans = make_spans_from_ranges(ss, [](const auto& e) { return gsl_lite::make_span(e.v); });
+ *  ```
+ *
+ *  The wrong lambda is returning a copy of member vector `v`, which is then deallocated.
+ *
+ */
+template <class T, class RangeOfRanges, class GetRangeCallback>
 std::vector<gsl_lite::span<T>> make_spans_from_ranges(
     RangeOfRanges&& r,
     GetRangeCallback&& get_range) {
 	std::vector<gsl_lite::span<T>> spans(r.size());
 	for (size_t i = 0; i < spans.size(); ++i) {
-		spans[i] = gsl_lite::make_span(get_range(r[i]).data(), get_range(r[i]).size());
+		spans[i] =
+		    gsl_lite::make_span(get_range(r[i]).data(), get_range(r[i]).size());
 	}
 	return spans;
 }
 
-template <class T,
-          class RangeOfRanges>
+template <class T, class RangeOfRanges>
 std::vector<gsl_lite::span<T>> make_spans_from_ranges(RangeOfRanges&& r) {
-    auto get_range = [](auto& c) {
-	        return gsl_lite::make_span(c);
-	};
-    return make_spans_from_ranges<T>(std::forward<RangeOfRanges>(r), get_range);
+	auto get_range = [](auto& c) { return gsl_lite::make_span(c); };
+	return make_spans_from_ranges<T>(std::forward<RangeOfRanges>(r), get_range);
 }
 
 template <class T, class RangeOfPtrs>
