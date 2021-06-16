614e6a1d5ad5c7ae0204bca8016f8860cbba36da
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-06-16 20:03:13 +0200

    Add container conversion test

---
+++ test/test-container-conversion.cu
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <vector>
+
+#include <gsl-lite/gsl-lite.hpp>
+
+#include <cuda/runtime_api.hpp>
+
+#include <Eigen/Sparse>
+
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <matrixmarket/eigen.hpp>
+
+#include <thrustshift/COO.h>
+#include <thrustshift/CSR.h>
+#include <thrustshift/equal.h>
+#include <thrustshift/container-conversion.h>
+#include <thrustshift/eigen3>
+
+namespace bdata = boost::unit_test::data;
+
+using namespace thrustshift;
+
+namespace {
+
+struct test_data_t {
+	std::vector<float> values;
+	std::vector<int> row_indices;
+	std::vector<int> col_indices;
+	size_t num_rows;
+	size_t num_cols;
+};
+
+inline std::ostream& operator<<(std::ostream& os, [[maybe_unused]] const test_data_t& td) {
+	return os;
+}
+
+const std::vector<test_data_t> test_data = {
+    // clang-format off
+	{
+		{1, 2, 3, 4},
+		{0, 1, 2, 3},
+		{1, 2, 3, 4},
+		7,
+		7
+	},
+	{
+		{1},
+		{3},
+		{3},
+		5,
+		6
+	},
+	{
+		{1, 1, 1},
+		{3, 3, 3},
+		{3, 3, 3},
+		5,
+		6
+	},
+	{
+		{},
+		{},
+		{},
+		0,
+		0
+	},
+	{
+		{0},
+		{0},
+		{0},
+		1,
+		1
+	},
+	{
+		{1, 2, 3, 4},
+		{0, 0, 0, 3},
+		{1, 2, 3, 3},
+		4,
+		4
+	},
+	{
+		{1, 2, 3, 4},
+		{0, 0, 0, 3},
+		{3, 2, 1, 3},
+		4,
+		4
+	},
+
+    // clang-format on
+};
+
+} // namespace
+
+BOOST_DATA_TEST_CASE(test_container_conversion, test_data, td) {
+	thrustshift::COO<float, int> coo(
+	    td.values, td.row_indices, td.col_indices, td.num_rows, td.num_cols);
+	coo.change_storage_order(thrustshift::storage_order_t::row_major);
+	pmr::delayed_pool_type<pmr::managed_resource_type> delayed_memory_resource;
+	auto csr = coo2csr<CSR<float, int>>(coo, delayed_memory_resource);
+	auto coo2 = csr2coo<COO<float, int>>(csr, delayed_memory_resource);
+
+	const bool b = coo2 == coo;
+	BOOST_TEST(b);
+}
+
+BOOST_AUTO_TEST_CASE(test_container_conversion_matrix_store) {
+	const std::string pth = "/mnt/matrix_store/ModelPDEs/A_problem4n256square/A_problem4n256square.mtx";
+	Eigen::SparseMatrix<float> eigen_mtx = matrixmarket::readEigenSparseFromFile<float>(pth);
+	pmr::delayed_pool_type<pmr::managed_resource_type> delayed_memory_resource;
+	auto csr = eigen::sparse_mtx2csr(eigen_mtx);
+	auto coo = csr2coo<COO<float, int>>(csr, delayed_memory_resource);
+	auto csr2 = coo2csr<CSR<float, int>>(coo, delayed_memory_resource);
+	const bool b = csr == csr2;
+	BOOST_TEST(b);
+}
