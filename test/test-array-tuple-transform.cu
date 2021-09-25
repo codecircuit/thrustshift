7764b59f318c7b567eeabfb5c73ad52587f508a1
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-09-25 20:30:16 +0200

    Add tuple transform

--- test/test-array-tuple-transform.cu
+++ test/test-array-tuple-transform.cu
@@ -12,7 +12,7 @@
 namespace bdata = boost::unit_test::data;
 using namespace thrustshift;
 
-BOOST_AUTO_TEST_CASE(test_array_transform) {
+BOOST_AUTO_TEST_CASE(test_array_tuple_transform) {
 
 	std::array<int, 4> arr({1, 2, 3, 4});
 
@@ -20,4 +20,13 @@ BOOST_AUTO_TEST_CASE(test_array_transform) {
 	for (int i = 0; i < 4; ++i) {
 		BOOST_TEST(result[i] == (i + 1) * 2);
 	}
+
+	float a = 1;
+	float b = 2;
+	std::tuple<float, float> tup_ref(a, b);
+
+	auto tup_result = tuple::transform(tup_ref, [](float f) { return f * 2; });
+
+	BOOST_TEST(std::get<0>(tup_result) == 2);
+	BOOST_TEST(std::get<1>(tup_result) == 4);
 }
