533ed68f6aa201f54b9622dcaa203fdf6844b466
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2021-12-21 14:23:49 +0100

    Add Scan wrapper for CUB

---
+++ include/thrustshift/scan.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cuda/runtime_api.hpp>
+
+#include <gsl-lite/gsl-lite.hpp>
+
+#include <cub/cub.cuh>
+
+#include <thrustshift/not-a-vector.h>
+
+namespace thrustshift {
+
+namespace async {
+
+template <class ValuesInRange,
+          class ValuesOutRange,
+          class NumSelectedPtr,
+          class ScanOp,
+          class MemoryResource>
+void inclusive_scan(cuda::stream_t& stream,
+                    ValuesInRange&& values_in,
+                    ValuesOutRange&& values_out,
+                    ScanOp scan_op,
+                    MemoryResource& delayed_memory_resource) {
+
+	const std::size_t N = values_in.size();
+
+	gsl_Expects(values_out.size() == N);
+
+	size_t tmp_bytes_size = 0;
+	void* tmp_ptr = nullptr;
+
+	auto exec = [&] {
+		cuda::throw_if_error(cub::DeviceScan::InclusiveScan(tmp_ptr,
+		                                                    tmp_bytes_size,
+		                                                    values_in.data(),
+		                                                    values_out.data(),
+		                                                    scan_op,
+		                                                    N,
+		                                                    stream.id()));
+	};
+	exec();
+	auto tmp =
+	    make_not_a_vector<uint8_t>(tmp_bytes_size, delayed_memory_resource);
+	tmp_ptr = tmp.to_span().data();
+	exec();
+}
+
+} // namespace async
+
+} // namespace thrustshift
