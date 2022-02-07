06344807f731e6386ff19f16efc1ae8953263ee3
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2022-02-07 12:47:21 +0100

    Add wrappers for CUB's segmented reduce

--- include/thrustshift/reduction.h
+++ include/thrustshift/reduction.h
@@ -42,6 +42,81 @@ void reduce(cuda::stream_t& stream,
 	exec();
 }
 
+template <class RangeA,
+          class RangeB,
+          class RangeC,
+          class ReductionF,
+          class Init,
+          class MemoryResource>
+void segmented_reduce(cuda::stream_t& stream,
+                      RangeA&& values,
+                      RangeB&& result,
+                      RangeC&& segment_ptrs,
+                      ReductionF reduction_functor,
+                      Init initial_value,
+                      MemoryResource& delayed_memory_resource) {
+
+	gsl_Expects(segment_ptrs.size() > 0);
+
+	size_t tmp_bytes_size = 0;
+	void* tmp_ptr = nullptr;
+	auto exec = [&] {
+		cuda::throw_if_error(cub::DeviceSegmentedReduce::Reduce(
+		    tmp_ptr,
+		    tmp_bytes_size,
+		    values.data(),
+		    result.data(),
+		    gsl_lite::narrow<int>(segment_ptrs.size() - 1),
+		    segment_ptrs.data(),
+		    segment_ptrs.data() + 1,
+		    reduction_functor,
+		    initial_value,
+		    stream.id()));
+	};
+	exec();
+	auto tmp =
+	    make_not_a_vector<uint8_t>(tmp_bytes_size, delayed_memory_resource);
+	tmp_ptr = tmp.to_span().data();
+	exec();
+}
+
+template <class ItA,
+          class ItB,
+          class ItC,
+          class ReductionF,
+          class Init,
+          class MemoryResource>
+void segmented_reduce(cuda::stream_t& stream,
+                      ItA values,
+                      ItB result,
+                      ItC segment_ptrs,
+                      int num_segments,
+                      ReductionF reduction_functor,
+                      Init initial_value,
+                      MemoryResource& delayed_memory_resource) {
+
+	size_t tmp_bytes_size = 0;
+	void* tmp_ptr = nullptr;
+	auto exec = [&] {
+		cuda::throw_if_error(
+		    cub::DeviceSegmentedReduce::Reduce(tmp_ptr,
+		                                       tmp_bytes_size,
+		                                       values,
+		                                       result,
+		                                       num_segments,
+		                                       segment_ptrs,
+		                                       segment_ptrs + 1,
+		                                       reduction_functor,
+		                                       initial_value,
+		                                       stream.id()));
+	};
+	exec();
+	auto tmp =
+	    make_not_a_vector<uint8_t>(tmp_bytes_size, delayed_memory_resource);
+	tmp_ptr = tmp.to_span().data();
+	exec();
+}
+
 } // namespace async
 
 } // namespace thrustshift
